4e167fc7486c2486ffa3c530896ba7aa436b807d
elartan <baur.roman@gmx.de>
2022-02-02 18:27:26 +0100

    employee.py added, remove/update employee not completely done

---
+++ pmm_backend/controllers/employee.py
@@ -0,0 +1,38 @@
+from pmm_backend import api, settings, db
+from pmm_backend.models import models
+from flask_restx import fields, marshal
+
+import json
+
+
+class EmployeeController:
+
+    @staticmethod
+    def add_employee(first_name, last_name):
+        employee = models.Employee(first_name=first_name, last_name=last_name)
+
+        db.session.add(employee)
+        db.session.commit()
+
+    @staticmethod
+    def list_employees():
+        marshaller = {
+            'employee_id': fields.Integer,
+            'first_name': fields.String,
+            'last_name': fields.String,
+        }
+
+        all_employees = models.Employee.query.all()
+        return json.dumps(marshal(all_employees, marshaller))
+
+    @staticmethod
+    def modify_employee(first_name, last_name):
+        employee = models.Employee(first_name=first_name, last_name=last_name)
+
+        db.session.add(employee)
+        db.session.commit()
+
+    @staticmethod
+    def delete_employee(employee_id):
+        models.Employee.query.filter_by(employee_id=employee_id).delete()
+        db.session.commit()
--- pmm_backend/routes.py
+++ pmm_backend/routes.py
@@ -1,5 +1,6 @@
 from pmm_backend import api
 from pmm_backend.controllers.user import UserController
+from pmm_backend.controllers.employee import EmployeeController
 from flask import redirect, request, session, escape
 
 
@@ -7,12 +8,14 @@ from flask import redirect, request, session, escape
 def list_users():
     return UserController.list_users()
 
+
 @api.route('/user/login', methods=['POST'])
 def login_user():
     email = request.form.get('email')
     password = request.form.get('password')
     status = UserController.try_login()
     return status
+
 
 @api.route('/user/add', methods=['POST'])
 def add_user():
@@ -24,4 +27,33 @@ def add_user():
 
     status = UserController.add_user(role_id=role_id, email=email, password=password, first_name=first_name,
                                      last_name=last_name)
+    return status
+
+
+@api.route('/employee/list')
+def list_employees():
+    return EmployeeController.list_employees()
+
+
+@api.route('/employee/add', methods=['POST'])
+def add_employee():
+    first_name = escape(request.form.get('first_name'))
+    last_name = escape(request.form.get('last_name'))
+
+    status = EmployeeController.add_employee(first_name=first_name, last_name=last_name)
+    return status
+
+
+@api.route('/employee/<employee_id>', methods=['PUT'])
+def modify_employee():
+    first_name = escape(request.form.get('first_name'))
+    last_name = escape(request.form.get('last_name'))
+
+    status = EmployeeController.modify_employee(first_name=first_name, last_name=last_name)
+    return status
+
+
+@api.route('/employee/<employee_id>', methods=['DELETE'])
+def delete_employee(employee_id):
+    status = EmployeeController.delete_employee(employee_id=employee_id)
     return status
