fbbfd8eb535c883661e14f1d7b8a63ed23d5f86c
kratz@timakramo.de <UHrQtgxXGfJJ3VFdV>
2022-02-02 12:47:35 +0100

    erste User Funktionen

--- pmm_backend/routes.py
+++ pmm_backend/routes.py
@@ -7,7 +7,7 @@ from flask import redirect, request, session, escape
 def list_users():
     return UserController.list_users()
 
-@api.route('/user/login')
+@api.route('/user/login', methods=['POST'])
 def login_user():
     email = request.form.get('email')
     password = request.form.get('password')
@@ -16,12 +16,12 @@ def login_user():
 
 @api.route('/user/add', methods=['POST'])
 def add_user():
-    role_id = request.form.get('role_id')
-
+    role_id = int(request.form.get('role_id'))
     email = escape(request.form.get('email'))
     password = request.form.get('password')
     first_name = escape(request.form.get('first_name'))
     last_name = escape(request.form.get('last_name'))
 
-    status = UserController.try_login()
+    status = UserController.add_user(role_id=role_id, email=email, password=password, first_name=first_name,
+                                     last_name=last_name)
     return status
