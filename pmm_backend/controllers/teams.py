1258c68d417926968183d6b5ba64d7d59f3a20ac
timm-k <ki9bot8DTYuVzZeRtc5jrRvKZgv5>
2022-02-04 16:56:07 +0100

    Initial TeamsController

---
+++ pmm_backend/controllers/teams.py
@@ -0,0 +1,11 @@
+from pmm_backend import api, settings, db
+from pmm_backend.models import models
+from flask_restx import fields, marshal
+
+import json
+
+
+class TeamsController:
+    @staticmethod
+    def add_team():
+        pass
