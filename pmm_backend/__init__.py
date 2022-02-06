74a43eeed1820f139df89921c3a4fd2ad435f0d1
timm-k <ki9bot8DTYuVzZeRtc5jrRvKZgv5>
2022-02-06 14:38:09 +0100

    CORS Fix

--- pmm_backend/__init__.py
+++ pmm_backend/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from pmm_backend import settings
 
 api = Flask(__name__)
 api.config.from_pyfile('settings.py')
+CORS(api)
 
 db = SQLAlchemy(api)
 
