4e7dfc8c48f0a64363b9094db165d956ef9fd50e
timm-k <ki9bot8DTYuVzZeRtc5jrRvKZgv5>
2022-02-25 09:57:37 +0100

    User Tests

--- pmm_backend/tests/run_tests.py
+++ pmm_backend/tests/run_tests.py
@@ -3,4 +3,5 @@ from pmm_backend.tests.test_user import TestUser
 
 client = api.test_client()
 
-TestUser.test_login(client)
+token = TestUser.test_login(client)
+TestUser.test_add_and_list_user(client, token)
--- pmm_backend/tests/test_user.py
+++ pmm_backend/tests/test_user.py
@@ -3,3 +3,16 @@ class TestUser:
     def test_login(client):
         response = client.post("/user/login",  data={"email": "test@test.com", "password": "ABC"})
         assert response.status_code == 201
+        assert response.json["message"] == "success"
+
+        return response.json["token"]
+
+    @staticmethod
+    def test_add_and_list_user(client, token):
+        data = {"email": "test_user@test.com",
+                "password": "test",
+                "first_name": "test_first_name",
+                "last_name": "test_last_name",
+                "role_id": 1}
+        client.post("/user", data=data, headers={"x-access-token": token})
+        client.get("/user/list", headers={"x-access-token": token})
