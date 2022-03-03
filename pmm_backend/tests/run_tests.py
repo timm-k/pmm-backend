5e49aee18a330adbe65bcee7539c2cfe9e4147a0
timm-k <ki9bot8DTYuVzZeRtc5jrRvKZgv5>
2022-03-03 17:23:22 +0100

    User Tests

--- pmm_backend/tests/run_tests.py
+++ pmm_backend/tests/run_tests.py
@@ -2,6 +2,13 @@ from pmm_backend import api
 from pmm_backend.tests.test_user import TestUser
 
 client = api.test_client()
-
 token = TestUser.test_login(client)
-TestUser.test_add_and_list_user(client, token)
+
+#######
+# Users
+#######
+
+user_id = TestUser.test_add_and_list_user(client, token)
+TestUser.test_remove_user(client, token, user_id)
+
+TestUser.test_add_user_missing_email(client, token)
--- pmm_backend/tests/test_user.py
+++ pmm_backend/tests/test_user.py
@@ -1,8 +1,10 @@
+import json
+
 class TestUser:
     @staticmethod
     def test_login(client):
         response = client.post("/user/login",  data={"email": "test@test.com", "password": "ABC"})
-        assert response.status_code == 201
+        assert response.status_code == 200
         assert response.json["message"] == "success"
 
         return response.json["token"]
@@ -14,5 +16,37 @@ class TestUser:
                 "first_name": "test_first_name",
                 "last_name": "test_last_name",
                 "role_id": 1}
-        client.post("/user", data=data, headers={"x-access-token": token})
-        client.get("/user/list", headers={"x-access-token": token})
+
+        response = client.post("/user", data=data, headers={"x-access-token": token})
+        user_id = response.json["user_id"]
+        assert response.status_code == 200
+        assert response.json["message"] == "success"
+
+        response = client.get("/user/list", headers={"x-access-token": token})
+        assert response.status_code == 200
+
+        user_list = json.loads(response.data)
+        for u in user_list:
+            if int(user_id) == int(u['user_id']):
+                assert u['first_name'] == "test_first_name"
+                assert u['role_id'] == 1
+
+        return user_id
+
+
+    @staticmethod
+    def test_remove_user(client, token, user_id):
+        response = client.delete("/user/" + str(user_id), headers={"x-access-token": token})
+        assert response.status_code == 200
+        assert response.json["message"] == "success"
+
+    @staticmethod
+    def test_add_user_missing_email(client, token):
+        data = {"password": "test",
+                "first_name": "test_first_name",
+                "last_name": "test_last_name",
+                "role_id": 1}
+
+        response = client.post("/user", data=data, headers={"x-access-token": token})
+        assert response.status_code == 400
+        assert "missing data" in response.json["message"]
